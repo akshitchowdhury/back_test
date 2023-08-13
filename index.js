7679813e6329f583f14b6f4459ebf99a12fca96f
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-08-14 03:58:15 +0530

    fourth

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ const port = 3000;
 app.listen(port, ()=>{
     console.log("listening to port "+ port);
 })
+
+app.get("/", (req,res)=>{
+    
+    const name = "DevAshura"
+
+    const agent = 700;
+    res.render()
+})
